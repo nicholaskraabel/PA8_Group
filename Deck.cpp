b94535e81512928d57b0f965018a3dcdef469c73
SebbsMercado <74687391+SebbsMercado@users.noreply.github.com>
2020-11-23 18:23:07 -0800

    Create Deck.cpp

---
+++ Deck.cpp
@@ -0,0 +1,45 @@
+#include"Deck.h"
+#include"ctime"
+
+Deck::Deck()
+{
+	string rank1[13] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+	for (int i = 0; i < 13; i++)
+	{
+		Card* Spades = new Card(rank1[i], "Spades");
+		deck.push_back(Spades);
+
+		Card* Clubs = new Card(rank1[i], "Clubs");
+		deck.push_back(Clubs);
+
+		Card* Diamonds = new Card(rank1[i], "Diamonds");
+		deck.push_back(Diamonds);
+
+		Card* Hearts = new Card(rank1[i], "Hearts");
+		deck.push_back(Hearts);
+	}
+
+}
+void Deck::shuffle()
+{
+	srand(time(NULL)); 
+	for (int j = 0; j < 100; j++)
+	{
+		for (int i = 0; i < deck.size(); i++)
+		{
+			int randCard = rand() % deck.size();
+			Card* card = deck[i];
+
+			deck[i] = deck[randCard];
+			deck[randCard] = card;
+		}
+	}
+}
+void Deck::showDeck()
+{
+	cout << "-----Deck-----" << endl;
+	for (int i = 0; i < deck.size(); i++)
+	{
+		cout << deck[i]->getSuit() << " of " << deck[i]->getRank() << endl;
+	}
+}
